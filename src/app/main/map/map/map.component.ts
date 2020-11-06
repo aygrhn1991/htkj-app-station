7a124033904f14cd2c6ab9371ecc92899b447beb
Chenyufeng <411610370@qq.com>
2020-11-06 11:30:17 +0800

    1

--- src/app/main/map/map/map.component.ts
+++ src/app/main/map/map/map.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { ToastService } from 'src/app/services/toast.service';
+import { UtilService } from 'src/app/services/util.service';
 declare var BMap: any;
 
 @Component({
@@ -18,7 +19,8 @@ export class MapComponent implements OnInit {
 
   constructor(private geolocation: Geolocation,
     private toast: ToastService,
-    private http: HttpClient) { }
+    private http: HttpClient,
+    private util: UtilService) { }
 
   ngOnInit() { }
 
@@ -31,32 +33,32 @@ export class MapComponent implements OnInit {
     this.goBack();
   }
   goBack() {
-    // let geolocation = new BMap.Geolocation();
-    // geolocation.getCurrentPosition((r) => {
-    //   let point = r.point;
-    //   let marker = new BMap.Marker(point);
-    //   this.map.addOverlay(marker);
-    //   this.map.centerAndZoom(point, 15);
-    // }, { enableHighAccuracy: true })
+    let geolocation = new BMap.Geolocation();
+    geolocation.getCurrentPosition((r) => {
+      let point = r.point;
+      let marker = new BMap.Marker(point);
+      this.map.addOverlay(marker);
+      this.map.centerAndZoom(point, 15);
+    }, { enableHighAccuracy: true })
 
-    this.geolocation.getCurrentPosition().then((resp) => {
-      let lng = resp.coords.longitude;
-      let lat = resp.coords.latitude;
-      let oldPoint = new BMap.Point(lng, lat);
-      let oldPointArray = [];
-      oldPointArray.push(oldPoint);
-      let convertor = new BMap.Convertor();
-      convertor.translate(oldPointArray, 1, 5, (data) => {
-        if (data.status === 0) {
-          let newPoint = data.points[0];
-          let marker = new BMap.Marker(newPoint);
-          this.map.addOverlay(marker);
-          this.map.centerAndZoom(newPoint, 15);
-        }
-      })
-    }).catch((error) => {
-      this.toast.show('地图处理异常，请重新进入应用');
-    });
+    // this.geolocation.getCurrentPosition().then((resp) => {
+    //   let lng = resp.coords.longitude;
+    //   let lat = resp.coords.latitude;
+    //   let oldPoint = new BMap.Point(lng, lat);
+    //   let oldPointArray = [];
+    //   oldPointArray.push(oldPoint);
+    //   let convertor = new BMap.Convertor();
+    //   convertor.translate(oldPointArray, 1, 5, (data) => {
+    //     if (data.status === 0) {
+    //       let newPoint = data.points[0];
+    //       let marker = new BMap.Marker(newPoint);
+    //       this.map.addOverlay(marker);
+    //       this.map.centerAndZoom(newPoint, 15);
+    //     }
+    //   })
+    // }).catch((error) => {
+    //   this.toast.show('地图处理异常，请重新进入应用');
+    // });
   }
   big() {
     this.map.zoomIn();
@@ -72,6 +74,13 @@ export class MapComponent implements OnInit {
       inOem: false,
       param: [lng, lat]
     }).subscribe((data: any) => {
+      data.data.forEach(x => {
+        let itemNum = this.util.getIntRandom(1, 5);
+        x.item = itemNum == 1 ? '胎压异常' : itemNum == 2 ? '转向助力异常' : itemNum == 3 ? '大灯故障' : itemNum == 4 ? '发动机异响' : '补胎';
+        x.score = this.util.getIntRandom(35, 50) / 10;
+        x.mile = this.util.getIntRandom(0, 50) / 10;
+        x.img = this.util.getIntRandom(1, 10);
+      });
       this.map.clearOverlays();
       this.dataList = data.data;
       this.dataTotal = data.data.length;
--- src/app/main/my-order/my-order/my-order.component.ts
+++ src/app/main/my-order/my-order/my-order.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UtilService } from 'src/app/services/util.service';
 
 @Component({
   selector: 'app-my-order',
@@ -8,14 +9,14 @@ import { Component, OnInit } from '@angular/core';
 export class MyOrderComponent implements OnInit {
 
   tab: number = 1;
-  list: Array<any> = [];
+  list: Array<any> = [
+    { order: '2020110610490001', state: '进行中', vm: '解放J6L 4x2', vno: '黑A67R01', time: '2020-11-06 13:00', user: '陈玉锋', phone: '15645100192', money: 300, item: '发动机异响，换机油' },
+    { order: '2020110610490002', state: '进行中', vm: '解放新J6P 6x2', vno: '黑A32ET3', time: '2020-11-06 09:00', user: '姜海峰', phone: '18945035587', money: 230, item: '转向助力过紧' },
+    { order: '2020110610490003', state: '已完成', vm: '一汽解放J6P 8x2', vno: '黑A66592', time: '2020-11-05 10:00', user: '刘明月', phone: '18655981208', money: 150, item: '补胎，轮胎校正' },
+  ];
 
-  constructor() { }
+  constructor(private util: UtilService) { }
 
-  ngOnInit() {
-    for (let i = 0; i < 10; i++) {
-      this.list.push(i);
-    }
-  }
+  ngOnInit() { }
 
 }
